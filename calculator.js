5576e1ce7f2df2e2ae78137181eb808fd778c57c
neppur <191008508+neppur@users.noreply.github.com>
2025-11-13 20:11:50 +0100

    Add clickevents for number buttons

--- calculator.js
+++ calculator.js
@@ -3,6 +3,23 @@ let numberOne;
 let numberTwo;
 let operator;
 
+let result = document.querySelector(".result");
+result.textContent = ""
+
+let resultStart = document.querySelector(".result-start");
+
+const buttons = document.querySelectorAll(".numbers button");
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        resultStart.remove();
+
+        if (button.textContent == "clear"){
+            location.reload();
+        } else {
+            const value = button.textContent.trim()
+            result.append(value);}
+    })
+});
 
 // Basic operations
 function add(x, y) {
